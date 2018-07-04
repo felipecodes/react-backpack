cd0be98fc11b8e6c4a6d02441c37f3630736dc02
Felipe Oliveira <felipe.o.silva18@gmail.com>
2018-07-04 19:57:37 -0300

    add koa-static

--- hyperion/index.js
+++ hyperion/index.js
@@ -3,10 +3,16 @@ const debug = require('debug')('hyperion');
 debug('Hyperion starting...');
 debug('logging with debug enabled');
 
+import path from 'path';
 import Koa from 'koa';
+import serve from 'koa-static';
 import renderer from './renderer';
 
 const app = new Koa();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(serve(path.join(__dirname, '..', 'dist')));
+}
 
 app.use(renderer);
 
